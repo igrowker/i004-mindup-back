90656c94028feb0f9b47af0f4530b23e57d84cf0
Emanuel Magallanes <ema_magallanes@hotmail.com>
2024-11-15 11:41:34 -0300

    Entity relations added, need to be impoved

--- core/src/main/java/com/mindup/core/entities/AppointmentEntity.java
+++ core/src/main/java/com/mindup/core/entities/AppointmentEntity.java
@@ -2,24 +2,27 @@ package com.mindup.core.entities;
 
 import com.mindup.core.enums.AppointmentStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
+@Builder
 public class AppointmentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // *patient id* //*
-    // *psychologist id* //*
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private User patient;
+    @ManyToOne
+    @JoinColumn(name = "psychologist_id")
+    private User psychologist;
     @Column(nullable = false)
-    private LocalDate date;
+    private LocalDateTime date;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AppointmentStatus status;
