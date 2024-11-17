7eb437fb9e06b8e70336a0e28b846e82db8a4a2d
Emanuel Magallanes <ema_magallanes@hotmail.com>
2024-11-16 23:34:38 -0300

    Configuring WebSockets

---
+++ chat/src/main/java/com/mindup/chat/config/WebSocketConfig.java
@@ -0,0 +1,40 @@
+package com.mindup.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ExecutorSubscribableChannel;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker (MessageBrokerRegistry registry){
+        //Simple broker just for testing, it will change to an external broker soon.
+        registry.enableSimpleBroker("/topic", "/queue");
+        //prefixes setted for messages sended from service to controller.
+        registry.setApplicationDestinationPrefixes("/chat");
+    }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //Stomp endpoint registed.
+        registry.addEndpoint("/ws").setAllowedOrigins("*")
+                .withSockJS();//this can be deleted only if the app It's for mobile and only mobile
+    }
+    //defining the exit channel
+    @Bean
+    public MessageChannel clientOutboundChannel(){
+        return new ExecutorSubscribableChannel();
+    }
+    //using bean because autowired gives problems in Junit, and this help to inject the dependency in controller;
+    @Bean
+    public SimpMessagingTemplate simpMessagingTemplate(MessageChannel clientOutboundChannel){
+        return new SimpMessagingTemplate(clientOutboundChannel);
+    }
+}
