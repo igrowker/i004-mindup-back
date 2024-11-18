864d4ecc37f53d5fe07a64efba1b25df6cf1e4c9
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-11-18 15:39:40 -0300

    entities

--- core/src/main/java/com/mindup/core/entities/User.java
+++ core/src/main/java/com/mindup/core/entities/User.java
@@ -2,6 +2,7 @@ package com.mindup.core.entities;
 
 import com.mindup.core.enums.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -12,17 +13,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+    
+    @NotBlank(message = "Name is required")
     @Column(nullable = false)
     private String name;
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     @Column(nullable = false, unique = true)
     private String email;
+    
+    @NotBlank(message = "Password is required")
     @Column(nullable = false)
     private String password;
+    
+    @NotNull(message = "Role is required")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+    
     @Column
     private String preferences;
+    
     @Column
     private String profile;
     private boolean availability;
