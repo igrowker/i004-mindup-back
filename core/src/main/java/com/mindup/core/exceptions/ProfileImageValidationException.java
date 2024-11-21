c5c36173c55bea4ed61f54063888bb1e6f819508
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-11-21 20:42:54 -0300

    first commit

---
+++ core/src/main/java/com/mindup/core/exceptions/ProfileImageValidationException.java
@@ -0,0 +1,8 @@
+package com.mindup.core.exceptions;
+
+public class ProfileImageValidationException extends RuntimeException {
+
+    public ProfileImageValidationException(String message) {
+        super(message);
+    }
+}
