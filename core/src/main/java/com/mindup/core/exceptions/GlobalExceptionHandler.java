c4dc33e233ba7e10fdf5ddcd3dafffa093e73992
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-11-23 22:50:41 -0300

    exception

--- core/src/main/java/com/mindup/core/exceptions/GlobalExceptionHandler.java
+++ core/src/main/java/com/mindup/core/exceptions/GlobalExceptionHandler.java
@@ -49,4 +49,12 @@ public class GlobalExceptionHandler {
         response.put("message", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
+
+    @ExceptionHandler(PhoneNumberFormatException.class)
+    public ResponseEntity<Map<String, String>> handlePhoneNumberFormatException(PhoneNumberFormatException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Invalid Phone Number Format");
+        response.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }
