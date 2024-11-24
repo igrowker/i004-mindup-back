f9a2ef47fc2b37ce59de3060211d1a133b5548a3
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-11-23 22:50:24 -0300

    mappers

--- core/src/main/java/com/mindup/core/mappers/UserMapper.java
+++ core/src/main/java/com/mindup/core/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.mindup.core.mappers;
 import com.mindup.core.dtos.User.UserDTO;
+import com.mindup.core.dtos.User.UserProfileDTO;
 import com.mindup.core.dtos.User.UserRegisterDTO;
 import org.mapstruct.Mapper;
 import com.mindup.core.entities.User;
@@ -8,4 +9,5 @@ import com.mindup.core.entities.User;
 public interface UserMapper {
     User toUser(UserRegisterDTO dto);
     UserDTO toUserDTO(User user);
+    UserProfileDTO toUserProfileDTO(User user);
 }
