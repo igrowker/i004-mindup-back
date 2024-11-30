2395dabe4407d569ddaca3410e8b627961f7760c
UlisesGS <ulisesgadea28@gmail.com>
2024-11-30 13:59:06 -0300

    application-properties finish, chat validations in process

--- chat/src/main/java/com/mindup/chat/services/implementations/MessageServiceImpl.java
+++ chat/src/main/java/com/mindup/chat/services/implementations/MessageServiceImpl.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Queue;
 
 @Service
@@ -47,7 +48,12 @@ public class MessageServiceImpl implements MessageService {
     @Transactional
     @Override
     public void joinProfessional(String professionalId) throws IOException {
+
         AvailablePsychologists professional = new AvailablePsychologists();
+
+        if(availablePsychologistsRepository.existsByProfessionalId(professionalId)){
+            availablePsychologistsRepository.deleteByProfessionalId(professionalId);
+        }
         professional.setProfessionalId(professionalId);
         professional.setTimestamp(LocalDateTime.now());
         availablePsychologistsRepository.save(professional);
@@ -62,13 +68,19 @@ public class MessageServiceImpl implements MessageService {
     @Override
     public TemporalChatDto requestChat(String patientId) throws IOException {
         //TODO. validar id de patient -> ULI. Otro Feign que en lugar de buscar por psico busque por paciente.
-        coreFeignClient.findPatientByUserIdAndRole(patientId);
+        try {
+            coreFeignClient.findPatientByUserIdAndRole(patientId);//si no pongo el runtimeexception no lo toma
+        }catch (RuntimeException e){ //agregar exception que no es el mismo usuario el que hace la petixion con el id
+            throw new ResourceNotFoundException("No existe el id del usuario.");
+        }
         TemporalChat temporalChat = new TemporalChat();
         temporalChat.setPatientId(patientId);
-        AvailablePsychologists professional = availablePsychologistsRepository.findAll().get(0);
-        if(professional==null){
-           var numbers= scraper.getEmergencyContactList();
-           throw new ResourceNotFoundException("There's no available psychologist. For further help you can call to these numbers: " + numbers);
+        AvailablePsychologists professional;
+        try {
+            professional = availablePsychologistsRepository.findAll().get(0);
+        } catch (RuntimeException e) {
+            var numbers = scraper.getEmergencyContactList();
+            throw new ResourceNotFoundException("There's no available psychologist. For further help you can call to these numbers: " + numbers);
         }
         temporalChat.setProfessionalId(professional.getProfessionalId());
         TemporalChat temporalChat1=temporalChatRepository.save(temporalChat);
@@ -83,11 +95,17 @@ public class MessageServiceImpl implements MessageService {
     //Endpoint 3: Corrobora que sea profesional y settea su id en el temporalChatDb. Esto sirve para pasar luego a la DB permanente (scheduler).
     @Transactional
     @Override
-    public Boolean professionalAccepted(TemporalChatDto temporalChatDto) { // NOS QUEDAMOS ACA GUILLE!!!!!!
-        coreFeignClient.findProfessionalByUserIdAndRole(temporalChatDto.professionalId());
+    public Boolean professionalAccepted(TemporalChatDto temporalChatDto) {
+        try {
+            coreFeignClient.findProfessionalByUserIdAndRole(temporalChatDto.professionalId());
+        }catch (RuntimeException e){ //agregar exception que no es el mismo usuario el que hace la petixion con el id
+            throw new ResourceNotFoundException("No existe el id del usuario.");
+        }
+        //validar temporalChat
         TemporalChat temporalChat = temporalChatRepository.findById(temporalChatDto.temporalChatId())
                 .orElseThrow(() -> new ResourceNotFoundException("Chat not found with id: " + temporalChatDto.temporalChatId()));
         temporalChat.setProfessionalId(temporalChatDto.professionalId());
+        temporalChatRepository.save(temporalChat);
         return true;
     }
 
