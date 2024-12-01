9d50d64e724ca68f2e6b625d989e51346d1389d8
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-11-30 21:50:15 -0300

    Repository act preferences

--- core/src/main/java/com/mindup/core/repositories/UserRepository.java
+++ core/src/main/java/com/mindup/core/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package com.mindup.core.repositories;
 
 import com.mindup.core.entities.User;
-import com.mindup.core.enums.Role;
+import com.mindup.core.enums.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
@@ -16,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     @Query("SELECT u FROM User u WHERE u.id = :userId AND u.role = :role")
     Optional<User> findUserByUserIdAndRole(String userId, Role role);
 
+    @Query("SELECT u FROM User u WHERE u.role = 'PSYCHOLOGIST' "
+            + "AND (:isBelow35 IS NULL OR (:isBelow35 = true AND u.age < 35) OR (:isBelow35 = false AND u.age >= 35)) "
+            + "AND (:gender IS NULL OR u.gender = :gender)")
+    List<User> findPsychologistsByPreferences(Boolean isBelow35, Gender gender);
 }
