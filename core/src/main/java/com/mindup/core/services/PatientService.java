5a988b0247abdc4afa78f903b6da3036b67e9f93
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-12-01 22:47:50 -0300

    PatientService

--- core/src/main/java/com/mindup/core/services/PatientService.java
+++ core/src/main/java/com/mindup/core/services/PatientService.java
@@ -2,8 +2,9 @@ package com.mindup.core.services;
 
 import com.mindup.core.dtos.User.PatientPreferencesDTO;
 import com.mindup.core.entities.User;
+import com.mindup.core.enums.Gender;
+import com.mindup.core.exceptions.InvalidPreferencesException;
 import com.mindup.core.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,12 +12,24 @@ import java.util.List;
 @Service
 public class PatientService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public PatientService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void validatePreferences(PatientPreferencesDTO preferencesDTO) {
+        if (preferencesDTO.getIsBelow35() == null) {
+            throw new InvalidPreferencesException("Age preference cannot be null.");
+        }
+        if (preferencesDTO.getGender() == null) {
+            throw new InvalidPreferencesException("Gender preference cannot be null.");
+        }
+    }
 
     public List<User> searchPsychologists(PatientPreferencesDTO preferencesDTO) {
-        return userRepository.findPsychologistsByPreferences(
-                preferencesDTO.getIsBelow35(),
-                preferencesDTO.getGender());
+        Boolean isBelow35 = preferencesDTO.getIsBelow35();
+        Gender gender = preferencesDTO.getGender();
+        return userRepository.findPsychologistsByPreferences(isBelow35, gender);
     }
 }
